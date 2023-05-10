ec99df136fa1e13cef54cb338315ed7cbe02e88e
Anil Rumdali Rai <anil.rai704757@gmail.com>
2023-05-10 08:21:29 +0545

    First Commit

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+my_dict= {"Name":"Anil","Address":"Tarahara","Age":"20","Gender":"Male"}
+print(my_dict)          #Print above value
+print(my_dict["Name"])  #Prints only given varaible 
+my_dict["Sex"]="Male"   #Update above dictionary
+print(my_dict)          
+my_dict["Age"]="19"       #Changes the varaible
+print(my_dict)
+del my_dict["Age"]      #Deletes varaible
